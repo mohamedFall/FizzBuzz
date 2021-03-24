7116e05da822ac6e1589a2de565cb4d01709e30c
mohamedFall <mohamed.fall@epsi.fr>
2021-03-24 22:36:42 +0100

    test_should_repeate_number_when_number_is_not_good_for_other_cases

--- fizzbuzz_checker.py
+++ fizzbuzz_checker.py
@@ -2,9 +2,13 @@ class FizzbuzzChecker:
 
     @staticmethod
     def fizzbuzz(number):
-        if(number <= 0):
-            raise(ValueError)
-        elif(number % 3 == 0):
+        if number <= 0:
+            raise ValueError
+        elif number % 3 == 0 and number % 5 == 0:
+            return "fizzbuzz"
+        elif number % 3 == 0:
             return "fizz"
-        elif(number % 5 == 0):
+        elif number % 5 == 0:
             return "buzz"
+        else:
+            return number
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -18,3 +18,11 @@ class FizzbuzzTest(unittest.TestCase):
     def test_should_be_buzz_when_number_multiple_of_5(self):
         actual = FizzbuzzChecker.fizzbuzz(25)
         self.assertEqual(actual, "buzz")
+
+    def test_should_be_fizzbuzz_when_number_multiple_of_3_and_5(self):
+        actual = FizzbuzzChecker.fizzbuzz(45)
+        self.assertEqual(actual, "fizzbuzz")
+
+    def test_should_repeate_number_when_number_is_not_good_for_other_cases(self):
+        actual = FizzbuzzChecker.fizzbuzz(49)
+        self.assertEqual(actual, 49)
