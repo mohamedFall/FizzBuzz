1434585331c590d2ceb99844a8b7e96e4300a4bb
mohamedFall <mohamed.fall@epsi.fr>
2021-03-29 09:55:10 +0200

    last commit

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -4,12 +4,9 @@ from fizzbuzz_checker import FizzbuzzChecker
 
 class FizzbuzzTest(unittest.TestCase):
 
-    def test_should_raise_exception_with_number_0(self):
+    def test_should_raise_exception_with_number_0_and_negative_numbers(self):
         with self.assertRaises(ValueError):
             FizzbuzzChecker.fizzbuzz(0)
-
-    def test_should_raise_exception_with_negative_numbers(self):
-        with self.assertRaises(ValueError):
             FizzbuzzChecker.fizzbuzz(-1)
 
     def test_should_be_fizz_when_number_multiple_of_3(self):
